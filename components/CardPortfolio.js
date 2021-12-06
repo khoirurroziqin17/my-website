75d38ab61fb70949e3f23c06b5e3f8b866f90283
khoirurroziqin17 <khoirurroziqin951@gmail.com>
2021-12-06 10:19:30 +0700

    add portfolio data json

--- components/CardPortfolio.js
+++ components/CardPortfolio.js
@@ -1,12 +1,14 @@
 import { Icon } from "@iconify/react";
 
-export default function CardPortfolio({ image, title, techs }) {
+export default function CardPortfolio({ data }) {
+  const { title, image, altText, techs, sourceCode, demo } = data;
+
   return (
     <article className="flex space-x-3 py-3 sm:px-4">
       <img
         className="flex-none w-32 h-20 mt-2 object-cover rounded-md"
         src={image}
-        alt=""
+        alt={altText}
       />
       <div className="space-y-2">
         <h3 className="font-medium text-lg">{title}</h3>
@@ -18,11 +20,19 @@ export default function CardPortfolio({ image, title, techs }) {
           ))}
         </div>
         <div className="flex flex-wrap">
-          <a href="/" className="flex items-center space-x-2 mr-6">
+          <a
+            href={demo}
+            target="_blank"
+            className="flex items-center space-x-2 mr-6"
+          >
             <Icon icon="bx:bx-movie-play" className="text-lg" />
             <span>Demo</span>
           </a>
-          <a href="/" className="flex items-center space-x-2">
+          <a
+            href={`https://github.com/khoirurroziqin17${sourceCode}`}
+            target="_blank"
+            className="flex items-center space-x-2"
+          >
             <Icon icon="ci:github" className="text-lg" />
             <span>Source Code</span>
           </a>
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,15 @@
 import BodyLayout from "../components/BodyLayout";
 import CardPortfolio from "../components/CardPortfolio";
 import Layout from "../components/Layout";
+import portfolioData from "./../dataPortfolio.json";
 
 export default function Home() {
   return (
     <Layout>
       <BodyLayout>
-        <CardPortfolio
-          image="/img1.jpg"
-          title="Youtube Hompage Clone"
-          techs={["reactjs", "tailwindcss"]}
-        />
-        <CardPortfolio
-          image="/img2.png"
-          title="Facebook Homepage Clone"
-          techs={["nextjs", "tailwindcss"]}
-        />
-        <CardPortfolio
-          image="/img3.png"
-          title="Twitter Homepage Clone"
-          techs={["reactjs", "bootstrap"]}
-        />
-        <CardPortfolio
-          image="/img1.jpg"
-          title="Youtube Hompage Clone"
-          techs={["reactjs", "tailwindcss"]}
-        />
-        <CardPortfolio
-          image="/img2.png"
-          title="Facebook Homepage Clone"
-          techs={["nextjs", "tailwindcss"]}
-        />
-        <CardPortfolio
-          image="/img3.png"
-          title="Twitter Homepage Clone"
-          techs={["reactjs", "bootstrap"]}
-        />
+        {portfolioData.map((data) => (
+          <CardPortfolio key={data.id} data={data} />
+        ))}
       </BodyLayout>
     </Layout>
   );
